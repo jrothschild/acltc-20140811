3e2573d55df0363c53fd7a9785eece47745ce80e
Zach <briggszj@gmail.com>
2014-08-11 11:45:39 -0500

    initial commit

---
+++ js/game_ctrl.js
@@ -0,0 +1,30 @@
+(function () {
+  "use strict";
+
+
+  angular.module("app").controller("gameCtrl", function($scope) {
+    var lastShot = null;
+
+    $scope.rows = [{
+      name: "A",
+    },{
+      name: "B",
+    },{
+      name: "C",
+    }];
+
+    $scope.status = function () {
+      if (lastShot === null) {
+        return "You may fire when ready.";
+      } else if (lastShot === "A1") {
+        return "HIT!";
+      } else {
+        return "Miss";
+      }
+    };
+
+    $scope.fire = function (shot) {
+      lastShot = shot;
+    };
+  });
+})();
